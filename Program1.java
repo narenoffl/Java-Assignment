8cbd9c728d88860cc8e889ae7e4eafcb2e9a60ba
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-05-29 13:22:28 +0530

    Java Assignment1

---
+++ Program1.java
@@ -0,0 +1,11 @@
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		System.out.println(args[0] + " Technologies " + args[1]);
+	}
+
+}
+ Output : javac Assignment1.java
+          java Assignment1 ABC Chennai
+
+          ABC Technologies Chennai
