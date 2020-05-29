daec8b1c9bf6c095df0124dda2b59a46ea015142
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-05-29 13:23:35 +0530

    Java Assignment 1

---
+++ Program2.java
@@ -0,0 +1,14 @@
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome " + args[0]);
+
+	}
+
+}  
+
+Output: javac Assignment1.java
+        java Assignment1 Robert
+        Welcome Robert
+
+
