703888adb7da0afabf6efa8cd5186ad67fd698bb
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-06-06 18:12:07 +0530

    JavaAssignment3 / June5

---
+++ Box.java
@@ -0,0 +1,23 @@
+public class Box {
+    double h,w,d;
+
+     Box(double width,double height,double depth)
+    {
+        h=height;
+        w=width;
+        d=depth;
+    }
+    double volume()
+    { double v;
+    v=h*w*d;
+    return v;
+    }
+   
+    public static void main(String[] args) {
+       
+    Box bc = new Box(9.8,80.3,10.7);
+    System.out.println("Volume of box is " +bc.volume());
+
+    }
+
+}
